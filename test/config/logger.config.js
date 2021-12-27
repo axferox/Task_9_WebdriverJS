1b07470a10543442d2e313fe45d8c417d3b7c621
axferox <axferox@gmail.com>
2021-12-27 12:42:00 +0300

    Fixed tests drops due to the logger

--- test/config/logger.config.js
+++ test/config/logger.config.js
@@ -3,18 +3,18 @@ const { format, transports } = require("winston");
 const { combine, simple} = format;
 
 const logger = winston.createLogger({
+  exitOnError: false,
   level: "info",
   format: combine(simple()),
   transports: [
     new winston.transports.Console({ level: "info" }),
-    new winston.transports.File({ filename: "test/logs/error.logger.log", handleRejections: true, level: "error" }),
-    new winston.transports.File({ filename: "test/logs/combined.logger.log", handleRejections: true, level: "debug" }),
+    new winston.transports.File({ level: "error", filename: "test/logs/error.logger.log" }),
+    new winston.transports.File({ level: "debug", filename: "test/logs/combined.logger.log" }),
   ],
 });
 
 logger.exceptions.handle(new transports.File({ filename: "test/logs/error.logger.log" }));
 logger.rejections.handle(new transports.File({ filename: "test/logs/combined.logger.log" }));
-
 module.exports = {
   logger,
 };
